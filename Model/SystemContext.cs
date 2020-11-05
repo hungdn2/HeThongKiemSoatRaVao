df3b0df5b8727a6cc181f06a5be2f92139e520c7
DESKTOP-ENSGCDP\Thang <dangquocphongbni98@gmail.com>
2020-11-05 16:36:57 +0700

    commit

---
+++ Model/SystemContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class SystemContext : DbContext
+    {
+        public SystemContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+        public DbSet<Test> tests { get; set; }
+    }
+}
