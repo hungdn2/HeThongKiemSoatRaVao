b17d553ef2063710a3d5490e0f832dbc88411c43
hưng đinh <hungdngch15253@gmail.com>
2020-11-11 14:30:09 +0700

    fix GetCustomerPaingRequest

--- SystemManageOutCome.Application/Client/Catalog/IPublicCustomerRequest.cs
+++ SystemManageOutCome.Application/Client/Catalog/IPublicCustomerRequest.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 using SystemManageSolution.ViewModel.Catalog.Customer;
-using SystemManageSolution.ViewModel.Catalog.Customer.Public;
+using SystemManageSolution.ViewModel.Catalog.Customer.Manage;
 using SystemManageSolution.ViewModel.Common;
 
 namespace SystemManageOutCome.Application.Client.Catalog
 {
     public interface IPublicCustomerRequest
     {
-        PageViewModel<CustomerViewModel> GetAllByCustomerID(GetCustomerPagingRequest request);
+        PageViewModel<CustomerViewModel> GetAllByCustomerID(GetCustomerPaingRequest request);
     }
 }
